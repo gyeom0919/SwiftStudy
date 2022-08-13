cf6676faf96e33411785d9e99a45ac11a04ff496
gyeom0919 <leegyeom09@gmail.com>
2022-08-13 17:57:48 +0900

    인마고 취업현황 앱 기초단계

---
+++ InmagoEmployment/InmagoEmployment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InmagoEmployment
+//
+//  Created by 겸 on 2022/08/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
