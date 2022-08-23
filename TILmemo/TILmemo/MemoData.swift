1de960b4ba6377ab9f7f2e26978fd95199c63338
gyeom0919 <leegyeom09@gmail.com>
2022-08-23 20:44:47 +0900

    Scene 구현 및 MemoData 추가

---
+++ TILmemo/TILmemo/MemoData.swift
@@ -0,0 +1,21 @@
+//
+//  MemoData.swift
+//  TILmemo
+//
+//  Created by 겸 on 2022/08/23.
+//
+
+import Foundation
+import UIKit
+
+/* 사용자가 입력한 값을 저장하는 파일
+   앱 전체가 사용하는 데이터의 형식과 기준을 결정해줘야된다
+ */
+
+class MemoData {
+    var memoId : Int? // 데이터 식별
+    var title : String? // 메모 제목
+    var contents : String? // 내용
+    var image : UIImage? // 이미지
+    var tilDate : Date? // TIL 작성일
+}
---
+++ TILmemo/TILmemo/MemoWriteViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MemoWriteViewController.swift
+//  TILmemo
+//
+//  Created by 겸 on 2022/08/23.
+//
+
+import UIKit
+
+class MemoWriteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
