76b4acf19066651f042fc8d63069dff2d1088a85
gyeom0919 <leegyeom09@gmail.com>
2022-09-01 11:22:54 +0900

    App LaunchScreen + Icon
    Figma로 앱 아이콘 만들고 런치 스크린 변경 ( 1초 딜레이 주기 )

--- TILmemo/TILmemo/AppDelegate.swift
+++ TILmemo/TILmemo/AppDelegate.swift
@@ -14,6 +14,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // 1초 미루기
+        Thread.sleep(forTimeInterval: 1.0)
+        
         return true
     }
 
