69c8484fae8ac9af932238b749fb818179230492
gyeom0919 <leegyeom09@gmail.com>
2022-10-13 23:52:51 +0900

    화면 미리보기

---
+++ StackView/StackView/Components/MyCardView.swift
@@ -0,0 +1,13 @@
+//
+//  MyCardView.swift
+//  uikit-codebase-ui
+//
+//  Created by Jeff Jeong on 2022/08/17.
+//
+
+import Foundation
+import UIKit
+
+class MyCardView : UIView {
+}
+
--- StackView/StackView/ViewController.swift
+++ StackView/StackView/ViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    lazy var topStackView : UIStackView = {
+        let stackView = UIStackView()
+        stackView.spacing = 10
+        
+        return stackView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = UIColor.yellow
+        
+        print(#function)
     }
-
-
 }
 
+#if DEBUG
+
+import SwiftUI
+
+struct ViewControllerPresentable: UIViewControllerRepresentable {
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+    }
+    
+    func makeUIViewController(context: Context) -> some UIViewController {
+        ViewController()
+    }
+}
+
+
+struct ViewControllerPrepresentable_PreviewProvider : PreviewProvider {
+    static var previews: some View {
+        ViewControllerPresentable()
+            .previewDevice("iPhone 12 mini")
+            .previewDisplayName("iPhone 12 mini")
+            .ignoresSafeArea()
+    }
+}
+
+#endif
