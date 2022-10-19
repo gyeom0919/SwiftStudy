9b2cf979f3d925c0d7b3dd588e64b7125c7b075c
gyeom0919 <leegyeom09@gmail.com>
2022-10-19 20:53:34 +0900

    added Library (Fakery,Snapkit,Then)

---
+++ ScrollView/ScrollView/MainVC.swift
@@ -0,0 +1,19 @@
+//
+//  MainVC.swift
+//  ScrollView
+//
+//  Created by 겸 on 2022/10/19.
+//
+
+import Foundation
+import UIKit
+
+class MainVC : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.white
+    }
+    
+}
+
